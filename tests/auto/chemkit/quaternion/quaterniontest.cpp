0a62208fc6ee7f021b133c4ff8f65beac73864fe
Kyle Lutz <kyle.r.lutz@gmail.com>
2011-02-12 22:03:40 -0800

    Add tests for Quaternion
    This adds tests for the Quaternion class.

--- tests/auto/chemkit/quaternion/quaterniontest.cpp
+++ tests/auto/chemkit/quaternion/quaterniontest.cpp
@@ -22,12 +22,73 @@
 
 #include <QtTest>
 
+#include <chemkit/quaternion.h>
+
 class QuaternionTest : public QObject
 {
     Q_OBJECT
 
     private slots:
+        void basic();
+        void add();
+        void subtract();
+        void multiply();
+        void conjugate();
 };
+
+void QuaternionTest::basic()
+{
+    chemkit::Quaternion q(1, 2, 3, 4);
+    QCOMPARE(q.x(), 1.0);
+    QCOMPARE(q.y(), 2.0);
+    QCOMPARE(q.z(), 3.0);
+    QCOMPARE(q.r(), 4.0);
+
+    q = chemkit::Quaternion(5, 6, 7, 8);
+    QCOMPARE(q.x(), 5.0);
+    QCOMPARE(q.y(), 6.0);
+    QCOMPARE(q.z(), 7.0);
+    QCOMPARE(q.r(), 8.0);
+}
+
+void QuaternionTest::add()
+{
+    chemkit::Quaternion a(2, 3, 4, 5);
+    chemkit::Quaternion b(4, 5, 6, 7);
+    QVERIFY(a.add(b) == chemkit::Quaternion(6, 8, 10, 12));
+    QVERIFY(a + b == chemkit::Quaternion(6, 8, 10, 12));
+    QVERIFY(b.add(a) == chemkit::Quaternion(6, 8, 10, 12));
+    QVERIFY(b + a == chemkit::Quaternion(6, 8, 10, 12));
+}
+
+void QuaternionTest::subtract()
+{
+    chemkit::Quaternion a(2, 3, 4, 5);
+    chemkit::Quaternion b(4, 5, 6, 7);
+    QVERIFY(a.subtract(b) == chemkit::Quaternion(-2, -2, -2, -2));
+    QVERIFY(a - b == chemkit::Quaternion(-2, -2, -2, -2));
+    QVERIFY(b.subtract(a) == chemkit::Quaternion(2, 2, 2, 2));
+    QVERIFY(b - a == chemkit::Quaternion(2, 2, 2, 2));
+}
+
+void QuaternionTest::multiply()
+{
+    chemkit::Quaternion a(2, 3, 4, 5);
+    chemkit::Quaternion b(4, 5, 6, 7);
+    QVERIFY(a.multiply(b) == chemkit::Quaternion(32, 50, 56, -12));
+    QVERIFY(a * b == chemkit::Quaternion(32, 50, 56, -12));
+    QVERIFY(b.multiply(a) == chemkit::Quaternion(36, 42, 60, -12));
+    QVERIFY(b * a == chemkit::Quaternion(36, 42, 60, -12));
+}
+
+void QuaternionTest::conjugate()
+{
+    chemkit::Quaternion q(1, 2, 3, 4);
+    QVERIFY(q.conjugate() == chemkit::Quaternion(-1, -2, -3, 4));
+
+    q = chemkit::Quaternion(-2, -4, -6, -8);
+    QVERIFY(q.conjugate() == chemkit::Quaternion(2, 4, 6, -8));
+}
 
 QTEST_APPLESS_MAIN(QuaternionTest)
 #include "quaterniontest.moc"
