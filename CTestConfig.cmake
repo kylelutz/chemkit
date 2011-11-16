4eab6128e47c87a2d12a88ce601429ad9b64b8ff
Kyle Lutz <kyle.r.lutz@gmail.com>
2011-11-15 20:33:59 -0500

    Add CTestConfig file
    This adds a CTestConfig file for chemkit which
    enables testing results to be pushed to CDash.

---
+++ CTestConfig.cmake
@@ -0,0 +1,7 @@
+set(CTEST_PROJECT_NAME "chemkit")
+set(CTEST_NIGHTLY_START_TIME "23:59:59 UTC")
+
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=chemkit")
+set(CTEST_DROP_SITE_CDASH TRUE)
