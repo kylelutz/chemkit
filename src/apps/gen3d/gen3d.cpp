424cfd288e7722ec790f4f7c59b7bfc8fed76bfd
Kyle Lutz <kyle.r.lutz@gmail.com>
2011-12-16 19:50:40 -0500

    Add chemkit-gen3d application
    This adds a new command line application named chemkit-gen3d
    which predicts 3D coordinates for a molecule given its SMILES
    formula.

---
+++ src/apps/gen3d/gen3d.cpp
@@ -0,0 +1,192 @@
+/******************************************************************************
+**
+** Copyright (C) 2009-2011 Kyle Lutz <kyle.r.lutz@gmail.com>
+** All rights reserved.
+**
+** This file is a part of the chemkit project. For more information
+** see <http://www.chemkit.org>.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions
+** are met:
+**
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in the
+**     documentation and/or other materials provided with the distribution.
+**   * Neither the name of the chemkit project nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**
+******************************************************************************/
+
+#include <string>
+#include <iostream>
+
+#include <boost/scoped_ptr.hpp>
+#include <boost/program_options.hpp>
+#include <boost/algorithm/string.hpp>
+
+#include <chemkit/atom.h>
+#include <chemkit/point3.h>
+#include <chemkit/chemkit.h>
+#include <chemkit/foreach.h>
+#include <chemkit/forcefield.h>
+#include <chemkit/lineformat.h>
+#include <chemkit/moleculefile.h>
+
+void printHelp(char *argv[], const boost::program_options::options_description &options)
+{
+    std::cout << "Usage: " << argv[0] << " [OPTIONS] formula file\n";
+    std::cout << "\n";
+    std::cout << "Generates 3D coordinates for a molecule from its SMILES formula.\n";
+    std::cout << "\n";
+    std::cout << "Options:\n";
+    std::cout << options << "\n";
+}
+
+// Generate initial 3D coordinates
+void gen3d(chemkit::Molecule *molecule)
+{
+    foreach(chemkit::Atom *atom, molecule->atoms()){
+        atom->setPosition(molecule->size() * chemkit::Point3::Random().normalized());
+    }
+}
+
+// Optimize the 3D coordinates using the uff force field
+void optimizeGeometry(chemkit::Molecule *molecule)
+{
+    boost::scoped_ptr<chemkit::ForceField> forceField(chemkit::ForceField::create("uff"));
+    if(!forceField){
+        return;
+    }
+
+    forceField->addMolecule(molecule);
+
+    bool ok = forceField->setup();
+    if(!ok){
+        return;
+    }
+
+    bool converged = false;
+    size_t maxSteps = 500;
+
+    while(!converged && maxSteps--){
+        converged = forceField->minimizationStep(1);
+    }
+
+    forceField->writeCoordinates(molecule);
+}
+
+int main(int argc, char *argv[])
+{
+    std::string inputFormula;
+    std::string inputFormatName;
+    std::string outputFileName;
+    std::string outputFormatName;
+
+    boost::program_options::options_description options;
+    options.add_options()
+        ("formula",
+            boost::program_options::value<std::string>(&inputFormula),
+            "The input formula.")
+        ("output-file",
+            boost::program_options::value<std::string>(&outputFileName),
+            "The output file.")
+        ("input-format,i",
+            boost::program_options::value<std::string>(&inputFormatName),
+            "Sets the input format.")
+        ("output-format,o",
+            boost::program_options::value<std::string>(&outputFormatName),
+            "Sets the output format.")
+        ("help,h",
+            "Shows this help message");
+
+    boost::program_options::positional_options_description positionalOptions;
+    positionalOptions.add("formula", 1).add("output-file", 1);
+
+    boost::program_options::variables_map variables;
+    boost::program_options::store(
+        boost::program_options::command_line_parser(argc, argv)
+            .options(options)
+            .positional(positionalOptions).run(),
+        variables);
+    boost::program_options::notify(variables);
+
+    if(variables.count("help")){
+        printHelp(argv, options);
+        return 0;
+    }
+    else if(inputFormula.empty()){
+        printHelp(argv, options);
+        std::cerr << "Error: No input formula specified." << std::endl;
+        return -1;
+    }
+    else if(outputFileName.empty()){
+        printHelp(argv, options);
+        std::cerr << "Error: No output file specified." << std::endl;
+        return -1;
+    }
+
+    // check input format
+    inputFormatName = "smiles";
+
+    // create input line format
+    boost::scoped_ptr<chemkit::LineFormat> inputFormat(chemkit::LineFormat::create(inputFormatName));
+    if(!inputFormat){
+        if(inputFormatName.empty()){
+            std::cerr << "Failed to guess input format." << std::endl;
+        }
+        else{
+            std::cerr << "Input format: " << inputFormatName << " is not supported." << std::endl;
+        }
+
+        return -1;
+    }
+
+    // read input formula
+    chemkit::Molecule *molecule = inputFormat->read(inputFormula);
+    if(!molecule){
+        std::cerr << "Failed to parse formula: " << inputFormat->errorString() << std::endl;
+        return -1;
+    }
+
+    // generate 3d coordinates
+    gen3d(molecule);
+
+    // optimize coordinates
+    optimizeGeometry(molecule);
+
+    // set center to origin
+    molecule->setCenter(0, 0, 0);
+
+    // write output file
+    chemkit::MoleculeFile outputFile(outputFileName);
+    outputFile.addMolecule(molecule);
+    if(!outputFormatName.empty()){
+        if(!outputFile.setFormat(outputFormatName)){
+            std::cerr << "File format '" << outputFormatName << "' is not supported." << std::endl;
+            return -1;
+        }
+    }
+
+    if(!outputFile.write()){
+        std::cerr << "Error: failed to write output file: " << outputFile.errorString() << std::endl;
+        return -1;
+    }
+
+    return 0;
+}
---
+++ tests/auto/apps/gen3d/gen3dtest.cpp
@@ -0,0 +1,84 @@
+/******************************************************************************
+**
+** Copyright (C) 2009-2011 Kyle Lutz <kyle.r.lutz@gmail.com>
+** All rights reserved.
+**
+** This file is a part of the chemkit project. For more information
+** see <http://www.chemkit.org>.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions
+** are met:
+**
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in the
+**     documentation and/or other materials provided with the distribution.
+**   * Neither the name of the chemkit project nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**
+******************************************************************************/
+
+#include "gen3dtest.h"
+
+#include <chemkit/atom.h>
+#include <chemkit/molecule.h>
+#include <chemkit/moleculefile.h>
+
+const QString gen3dApplication = "../../../../bin/chemkit-gen3d";
+
+void Gen3dTest::benzene()
+{
+    // create temporary output file
+    QTemporaryFile output("XXXXXX.mol");
+    output.open();
+
+    // set arguments
+    QStringList arguments;
+    arguments.append("c1ccccc1");
+    arguments.append(output.fileName());
+
+    // run gen3d
+    QProcess process;
+    process.start(gen3dApplication, arguments);
+    process.waitForFinished();
+
+    // read output file
+    chemkit::MoleculeFile file(output.fileName().toStdString());
+    bool ok = file.read();
+    if(!ok)
+        qDebug() << file.errorString().c_str();
+    QVERIFY(ok);
+
+    // check formula
+    chemkit::Molecule *molecule = file.molecule();
+    QVERIFY(molecule != 0);
+    QCOMPARE(molecule->formula(), std::string("C6H6"));
+
+    // ensure the molecule has one ring
+    QCOMPARE(molecule->ringCount(), 1);
+
+    // ensure center point is at (0, 0, 0)
+    QVERIFY(molecule->center().isZero(0.01));
+
+    // ensure each atom position is not at (0, 0, 0)
+    foreach(const chemkit::Atom *atom, molecule->atoms()){
+        QVERIFY(!atom->position().isZero(0.01));
+    }
+}
+
+QTEST_APPLESS_MAIN(Gen3dTest)
