210ce8037998a6a9ab8e66b1617684fe7dd1c846
Kyle Lutz <kyle.r.lutz@gmail.com>
2012-03-17 18:47:42 -0400

    Add rule of five descriptors
    This adds two new molecular descriptors for calculating
    the number of violations and testing molecules against
    Lipinski's rule of five.

---
+++ src/plugins/rof5/ruleoffivedescriptor.cpp
@@ -0,0 +1,55 @@
+/******************************************************************************
+**
+** Copyright (C) 2009-2012 Kyle Lutz <kyle.r.lutz@gmail.com>
+** All rights reserved.
+**
+** This file is a part of the chemkit project. For more information
+** see <http://www.chemkit.org>.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions
+** are met:
+**
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in the
+**     documentation and/or other materials provided with the distribution.
+**   * Neither the name of the chemkit project nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**
+******************************************************************************/
+
+#include "ruleoffivedescriptor.h"
+
+#include <chemkit/molecule.h>
+
+RuleOfFiveDescriptor::RuleOfFiveDescriptor()
+    : chemkit::MolecularDescriptor("rule-of-five")
+{
+}
+
+chemkit::Variant RuleOfFiveDescriptor::value(const chemkit::Molecule *molecule) const
+{
+    int violations = 0;
+
+    if(molecule->descriptor("molecular-mass").toDouble() > 500.0) violations++;
+    if(molecule->descriptor("hydrogen-bond-donors").toInt() > 5) violations++;
+    if(molecule->descriptor("hydrogen-bond-acceptors").toInt() > 10) violations++;
+    if(molecule->descriptor("mlogp").toDouble() > 5.0) violations++;
+
+    return violations <= 1;
+}
