b3fe61080c9f5ea3880f44f55b4a585ac2ff51eb
Kyle Lutz <kyle.r.lutz@gmail.com>
2012-03-17 10:34:24 -0400

    Add MlogP descriptor
    This adds a new molecular descriptor plugin named mlogp which
    implements the Moriguchi method for calculating logP.

---
+++ src/plugins/mlogp/mlogpdescriptor.cpp
@@ -0,0 +1,290 @@
+/******************************************************************************
+**
+** Copyright (C) 2009-2012 Kyle Lutz <kyle.r.lutz@gmail.com>
+** All rights reserved.
+**
+** This file is a part of the chemkit project. For more information
+** see <http://www.chemkit.org>.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions
+** are met:
+**
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in the
+**     documentation and/or other materials provided with the distribution.
+**   * Neither the name of the chemkit project nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**
+******************************************************************************/
+
+#include "mlogpdescriptor.h"
+
+#include <chemkit/atom.h>
+#include <chemkit/bond.h>
+#include <chemkit/ring.h>
+#include <chemkit/foreach.h>
+#include <chemkit/molecule.h>
+
+// The MlogPDescriptor class calculates the Moriguchi logP
+// descriptor value for a given molecule.
+//
+// Reference: [Moriguchi 1992]
+MlogPDescriptor::MlogPDescriptor()
+    : chemkit::MolecularDescriptor("mlogp")
+{
+}
+
+// Returns the MlogP value for the molecule.
+chemkit::Variant MlogPDescriptor::value(const chemkit::Molecule *molecule) const
+{
+    // CX - summation of numbers of carbon and halogen atoms
+    //      weighted by C: 1.0, F: 0.5, Cl: 1.0, Br: 1.5, I: 2.0
+    chemkit::Real cx = 0;
+    foreach(const chemkit::Atom *atom, molecule->atoms()){
+        if(atom->is(chemkit::Atom::Carbon)){
+            cx += 1.0;
+        }
+        else if(atom->is(chemkit::Atom::Fluorine)){
+            cx += 0.5;
+        }
+        else if(atom->is(chemkit::Atom::Chlorine)){
+            cx += 1.0;
+        }
+        else if(atom->is(chemkit::Atom::Bromine)){
+            cx += 1.5;
+        }
+        else if(atom->is(chemkit::Atom::Iodine)){
+            cx += 2.0;
+        }
+    }
+
+    // NO - total number of nitrogen and oxygen atoms
+    chemkit::Real no = 0;
+    foreach(const chemkit::Atom *atom, molecule->atoms()){
+        if(atom->is(chemkit::Atom::Nitrogen) ||
+           atom->is(chemkit::Atom::Oxygen)){
+            no += 1.0;
+        }
+    }
+
+    // PRX - proximity effect of N/O, X-Y: 2.0, X-A-Y: 1.0
+    //       (X, Y: N/O, A: C, S or P) with a correction of
+    //       -1 for caroxamide/sulfonamide
+    chemkit::Real prx = 0;
+    foreach(const chemkit::Atom *atom, molecule->atoms()){
+        if(atom->is(chemkit::Atom::Carbon) ||
+           atom->is(chemkit::Atom::Sulfur) ||
+           atom->is(chemkit::Atom::Phosphorus)){
+            if(atom->isBondedTo(chemkit::Atom::Oxygen, chemkit::Bond::Double) &&
+               atom->isBondedTo(chemkit::Atom::Oxygen, chemkit::Bond::Single)){
+                prx += 2.0;
+            }
+            else if(atom->isBondedTo(chemkit::Atom::Nitrogen, chemkit::Bond::Double) &&
+                    atom->isBondedTo(chemkit::Atom::Nitrogen, chemkit::Bond::Single)){
+                prx += 2 * (atom->neighborCount(chemkit::Atom::Nitrogen) - 1);
+            }
+            else if(atom->isBondedTo(chemkit::Atom::Oxygen, chemkit::Bond::Single) &&
+                    atom->isBondedTo(chemkit::Atom::Nitrogen, chemkit::Bond::Single)){
+                prx += 2.0;
+            }
+            else if(atom->isBondedTo(chemkit::Atom::Oxygen,
+                                     chemkit::Bond::Double)){
+                prx += atom->neighborCount(chemkit::Atom::Nitrogen);
+            }
+            else if(atom->isBondedTo(chemkit::Atom::Nitrogen, chemkit::Bond::Triple) &&
+                    atom->isBondedTo(chemkit::Atom::Nitrogen, chemkit::Bond::Single)){
+                prx += 1.0;
+            }
+            else if(atom->isBondedTo(chemkit::Atom::Oxygen) &&
+                    atom->isBondedTo(chemkit::Atom::Nitrogen)){
+                prx += 1.0;
+            }
+        }
+        else if(atom->is(chemkit::Atom::Oxygen) &&
+                atom->isBondedTo(chemkit::Atom::Nitrogen)){
+            prx += 2.0;
+        }
+    }
+
+    // UB - total number of unsaturated bonds except those in NO2
+    chemkit::Real ub = 0;
+    foreach(const chemkit::Bond *bond, molecule->bonds()){
+        if(!bond->is(chemkit::Bond::Single)){
+            ub += 1.0;
+        }
+    }
+
+    // HB - dummy variable for the presence of intramolecular
+    //      hydrogen bond
+    chemkit::Real hb = 0;
+
+    // POL - number of aromatic polar substituents (aromatic
+    //       substituents excluding Ar-CX2 and Ar-CX=C<, with
+    //       X: C or H)
+    chemkit::Real pol = 0;
+    foreach(const chemkit::Ring *ring, molecule->rings()){
+        if(ring->isAromatic()){
+            foreach(const chemkit::Bond *exocyclicBond, ring->exocyclicBonds()){
+                const chemkit::Atom *substituent = 0;
+                if(ring->contains(exocyclicBond->atom1())){
+                    substituent = exocyclicBond->atom2();
+                }
+                else{
+                    substituent = exocyclicBond->atom1();
+                }
+
+                if(substituent->is(chemkit::Atom::Nitrogen) ||
+                   substituent->is(chemkit::Atom::Oxygen) ||
+                   substituent->is(chemkit::Atom::Sulfur) ||
+                   substituent->is(chemkit::Atom::Fluorine) ||
+                   substituent->is(chemkit::Atom::Chlorine) ||
+                   substituent->is(chemkit::Atom::Bromine) ||
+                   substituent->is(chemkit::Atom::Iodine)){
+                    pol += 1.0;
+                }
+                else if(substituent->is(chemkit::Atom::Carbon)){
+                    if(substituent->isBondedTo(chemkit::Atom::Nitrogen,
+                                               chemkit::Bond::Double)){
+                        pol += 1.0;
+                    }
+                    else if(substituent->isBondedTo(chemkit::Atom::Oxygen,
+                                                    chemkit::Bond::Double)){
+                        pol += 1.0;
+                    }
+                    else if(substituent->isBondedTo(chemkit::Atom::Fluorine) ||
+                            substituent->isBondedTo(chemkit::Atom::Chlorine) ||
+                            substituent->isBondedTo(chemkit::Atom::Bromine) ||
+                            substituent->isBondedTo(chemkit::Atom::Iodine)){
+                        pol += 1.0;
+                    }
+                }
+            }
+        }
+    }
+
+    // AMP - amphoteric property. alpha-aminoacid: 1.0, aminobenzoic
+    //       acid: 0.5, pyridinecarboxylic acid: 0.5
+    chemkit::Real amp = 0;
+
+    // ALK - dummy variable for alkane, alkene, cycloalkane, or
+    //       cycloalkene (hydrocarbons with 0 or 1 double bond)
+    chemkit::Real alk = 0;
+
+    // RNG - dummy variable for the presence of ring structures
+    //       except benzene and its condensed rings (aromatic,
+    //       heteroaromatic, and hydrocarbon rings)
+    chemkit::Real rng = 0;
+    foreach(const chemkit::Ring *ring, molecule->rings()){
+        bool unsaturated = false;
+
+        foreach(const chemkit::Bond *bond, ring->bonds()){
+            if(bond->is(chemkit::Bond::Double)){
+                unsaturated = true;
+            }
+        }
+
+        if(!unsaturated || ring->isHeterocycle() || ring->size() != 6){
+            rng = 1.0;
+            break;
+        }
+    }
+
+    // QN - quaternary nitrogen: >N+<, 1.0; N oxide, 0.5
+    chemkit::Real qn = 0;
+    foreach(const chemkit::Atom *atom, molecule->atoms()){
+        if(atom->is(chemkit::Atom::Nitrogen) &&
+           atom->neighborCount() == 4){
+            qn = 1.0;
+            break;
+        }
+    }
+
+    // NO2 - number of nitro groups
+    chemkit::Real no2 = 0;
+    foreach(const chemkit::Atom *atom, molecule->atoms()){
+        if(atom->is(chemkit::Atom::Nitrogen) &&
+           atom->neighborCount() == 3 &&
+           atom->isBondedTo(chemkit::Atom::Oxygen, chemkit::Bond::Single) &&
+           atom->isBondedTo(chemkit::Atom::Oxygen, chemkit::Bond::Double)){
+            no2 += 1.0;
+
+            // remove one from unsaturated bond count
+            ub -= 1.0;
+        }
+    }
+
+    // NCS - isothiocyanato (-N=C=S), 1.0;
+    //       thiocyanato (-S-C#N), 0.5
+    chemkit::Real ncs = 0;
+    foreach(const chemkit::Atom *atom, molecule->atoms()){
+        if(atom->is(chemkit::Atom::Carbon)){
+            if(atom->isBondedTo(chemkit::Atom::Sulfur, chemkit::Bond::Double) &&
+               atom->isBondedTo(chemkit::Atom::Nitrogen, chemkit::Bond::Single)){
+                ncs += 1.0;
+            }
+            else if(atom->isBondedTo(chemkit::Atom::Sulfur, chemkit::Bond::Single) &&
+                    atom->isBondedTo(chemkit::Atom::Nitrogen, chemkit::Bond::Triple)){
+                ncs += 0.5;
+            }
+        }
+    }
+
+    // BLM - dummy variable for the presence of beta-lactam
+    chemkit::Real blm = 0;
+    foreach(const chemkit::Ring *ring, molecule->rings()){
+        if(ring->size() == 4){
+            bool containsNitrogen = false;
+            bool containsCarbonyl = false;
+
+            foreach(const chemkit::Atom *atom, ring->atoms()){
+                if(atom->is(chemkit::Atom::Nitrogen)){
+                    containsNitrogen = true;
+                }
+                else if(atom->is(chemkit::Atom::Carbon)){
+                    if(atom->isBondedTo(chemkit::Atom::Oxygen,
+                                        chemkit::Bond::Double) &&
+                       atom->isBondedTo(chemkit::Atom::Nitrogen,
+                                        chemkit::Bond::Single)){
+                        containsCarbonyl = true;
+                    }
+                }
+            }
+
+            if(containsNitrogen && containsCarbonyl){
+                blm = 1.0;
+            }
+        }
+    }
+
+    // equation 4 in [Moriguchi 1992]
+    return 1.244 * pow(cx, 0.6) -
+           1.017 * pow(no, 0.9) +
+           0.406 * prx -
+           0.145 * pow(ub, 0.8) +
+           0.511 * hb +
+           0.268 * pol -
+           2.215 * amp +
+           0.912 * alk -
+           0.392 * rng -
+           3.684 * qn +
+           0.474 * no2 +
+           1.582 * ncs +
+           0.773 * blm -
+           1.041;
+}
---
+++ tests/auto/plugins/mlogp/mlogptest.cpp
@@ -0,0 +1,107 @@
+/******************************************************************************
+**
+** Copyright (C) 2009-2012 Kyle Lutz <kyle.r.lutz@gmail.com>
+** All rights reserved.
+**
+** This file is a part of the chemkit project. For more information
+** see <http://www.chemkit.org>.
+**
+** Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions
+** are met:
+**
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in the
+**     documentation and/or other materials provided with the distribution.
+**   * Neither the name of the chemkit project nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**
+******************************************************************************/
+
+#include "mlogptest.h"
+
+#include <boost/range/algorithm.hpp>
+
+#include <chemkit/molecule.h>
+#include <chemkit/moleculardescriptor.h>
+
+void MlogPTest::initTestCase()
+{
+    // verify that the mlogp plugin registered itself correctly
+    QVERIFY(boost::count(chemkit::MolecularDescriptor::descriptors(), "mlogp") == 1);
+}
+
+void MlogPTest::mlogp_data()
+{
+    QTest::addColumn<QString>("smilesString");
+    QTest::addColumn<QString>("formulaString");
+    QTest::addColumn<double>("mlogp");
+
+    // data from examples in [Moriguchi 1992]
+    QTest::newRow("halothane") << "C(C(F)(F)F)(Cl)Br" << "C2HBrClF3" << 2.60;
+    QTest::newRow("ampicillin") << "O=C(O)[C@@H]2N3C(=O)[C@@H](NC(=O)[C@@H](c1ccccc1)N)[C@H]3SC2(C)C" << "C16H19N3O4S" << 1.06;
+    QTest::newRow("valerolactone") << "CC1CCC(=O)O1" << "C5H8O2" << 0.60;
+    QTest::newRow("oxazepam") << "C1=CC=C(C=C1)C2=NC(C(=O)NC3=C2C=C(C=C3)Cl)O" << "C15H11ClN2O2" << 3.12;
+
+    // data from Table 2 in [Moriguchi 1994]
+    QTest::newRow("atropine") << "CN3[C@H]1CC[C@@H]3C[C@@H](C1)OC(=O)C(CO)c2ccccc2" << "C17H23NO3" << 2.21;
+    QTest::newRow("chloramphenicol") << "c1cc(ccc1[C@H]([C@@H](CO)NC(=O)C(Cl)Cl)O)[N+](=O)[O-]" << "C11H12Cl2N2O5" << 1.23;
+    //QTest::newRow("chlorothiazide") << "O=S(=O)(c1c(Cl)cc2c(c1)S(=O)(=O)/N=C\\N2)N" << "C7H6ClN3O4S2" << -0.36;
+    //QTest::newRow("chlorpromazine") << "CN(C)CCCN1c2ccccc2Sc3c1cc(cc3)Cl" << "C17H19ClN2S" << 3.77;
+    QTest::newRow("cimetidine") << "N#CN\\C(=N/C)NCCSCc1ncnc1C" << "C10H16N6S" << 0.82;
+    QTest::newRow("diazepam") << "CN1c2ccc(cc2C(=NCC1=O)c3ccccc3)Cl" << "C16H13ClN2O" << 3.36;
+    QTest::newRow("diltiazem") << "O=C2N(c3c(S[C@@H](c1ccc(OC)cc1)[C@H]2OC(=O)C)cccc3)CCN(C)C" << "C22H26N2O4S" << 2.67;
+    QTest::newRow("diphenhydramine") << "O(CCN(C)C)C(c1ccccc1)c2ccccc2" << "C17H21NO" << 3.26;
+    QTest::newRow("disopyramide") << "O=C(N)C(c1ncccc1)(c2ccccc2)CCN(C(C)C)C(C)C" << "C21H29N3O" << 2.47;
+    QTest::newRow("flufenamic acid") << "FC(F)(F)c1cc(ccc1)Nc2ccccc2C(=O)O" << "C14H10F3NO2" << 3.86;
+    QTest::newRow("haloperidol") << "c1cc(ccc1C(=O)CCCN2CCC(CC2)(c3ccc(cc3)Cl)O)F" << "C21H23ClFNO2" << 4.01;
+    QTest::newRow("imipramine") << "c1cc3c(cc1)CCc2c(cccc2)N3CCCN(C)C" << "C19H24N2" << 3.88;
+    QTest::newRow("lidocaine") << "O=C(Nc1c(cccc1C)C)CN(CC)CC" << "C14H22N2O" << 2.52;
+    QTest::newRow("phenobarbital") << "O=C1NC(=O)NC(=O)C1(c2ccccc2)CC" << "C12H12N2O3" << 0.78;
+    QTest::newRow("phenytoin") << "O=C2NC(=O)NC2(c1ccccc1)c3ccccc3" << "C15H12N2O2" << 1.80;
+    QTest::newRow("procainamide") << "O=C(c1ccc(N)cc1)NCCN(CC)CC" << "C13H21N3O" << 1.72;
+    QTest::newRow("propafenone") << "O=C(c1ccccc1OCC(O)CNCCC)CCc2ccccc2" << "C21H27NO3" << 3.00;
+    QTest::newRow("propranolol") << "CC(C)NCC(COc1cccc2c1cccc2)O" << "C16H21NO2" << 2.53;
+    QTest::newRow("tetracaine") << "O=C(OCCN(C)C)c1ccc(NCCCC)cc1" << "C15H24N2O2" << 2.64;
+    //QTest::newRow("trimethoprim") << "COc1cc(cc(c1OC)OC)Cc2cnc(nc2N)N" << "C14H18N4O3" << 1.26;
+    QTest::newRow("verapamil") << "N#CC(c1cc(OC)c(OC)cc1)(CCCN(CCc2ccc(OC)c(OC)c2)C)C(C)C" << "C27H38N2O4" << 3.23;
+}
+
+void MlogPTest::mlogp()
+{
+    QFETCH(QString, smilesString);
+    QFETCH(QString, formulaString);
+    QFETCH(double, mlogp);
+
+    QByteArray smiles = smilesString.toAscii();
+    QByteArray formula = formulaString.toAscii();
+
+    chemkit::Molecule molecule(smiles.constData(), "smiles");
+    QCOMPARE(molecule.formula().c_str(), formula.constData());
+
+    double tolerance = 0.1;
+    double actual = molecule.descriptor("mlogp").toDouble();
+    bool equal = std::abs(mlogp - actual) < tolerance;
+    if(!equal){
+        qDebug() << "Actual: " << actual;
+        qDebug() << "Expected: " << mlogp;
+    }
+    QVERIFY(equal);
+}
+
+QTEST_APPLESS_MAIN(MlogPTest)
